7c21d3d99a10986cccee5c0d6b853e9df1e04e44
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-10-25 22:22:17 +0100

    adding helmet and https

--- src/apps.js
+++ src/apps.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import helmet from "helmet"
 import planetRouter from "./routes/planets.router.js"
 import launchsRouter from "./routes/launchs.router.js";
 
 const app = express();
-app.use(express.json())
+app.use(helmet())
 app.use(cors());
+app.use(express.json())
 app.use(morgan("combined"))
 app.use("/planet", planetRouter)
 app.use("/launch",launchsRouter)
--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
 import http from "http";
 import app from "./apps.js";
+import fs from "fs";
 import { loadPlanetsData } from "./models/planets.model.js";
-import { connectDb } from "./Database/connectDb.js";
+import { connectDb } from "./services/connectDb.js";
 import "dotenv/config.js";
-import {loadLauchData} from "./models/launchs.model.js"
-const server = http.createServer(app);
+import { loadLauchData } from "./models/launchs.model.js";
+const server = http.createServer(
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },
+  app
+);
 const start = async () => {
   try {
     await connectDb(process.env.MONGO_URL);
     await loadPlanetsData();
-    await loadLauchData()
+    await loadLauchData();
     server.listen(process.env.PORT, () => {
       console.log("connected on port 3000");
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-start()
+start();
